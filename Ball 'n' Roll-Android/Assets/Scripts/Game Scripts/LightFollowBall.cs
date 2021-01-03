6b426510ce0fea9a9f4e109ac5d255fd0b5b72f3
Bifrost19 <miroto1234@abv.bg>
2021-01-03 02:20:58 +0200

    Remove the fog and fix light issues

---
+++ Ball 'n' Roll-Android/Assets/Scripts/Game Scripts/LightFollowBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowBall : MonoBehaviour
+{
+    public Transform BallPlayerLight;
+    public Vector3 offset;
+    public GameObject ballPlayerNull1;
+
+    void Update()
+    {
+
+        if (ballPlayerNull1 != null)
+        {
+            transform.position = BallPlayerLight.position + offset;
+        }
+
+    }
+}
