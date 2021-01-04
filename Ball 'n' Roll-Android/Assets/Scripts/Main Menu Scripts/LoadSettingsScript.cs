f573716e6a52c95ee460876312321d5ca172e48a
Relaxo143 <relaxo143@protonmail.com>
2021-01-04 19:55:01 +0200

    Ask for Storage Permissions

--- Ball 'n' Roll-Android/Assets/Scripts/Main Menu Scripts/LoadSettingsScript.cs
+++ Ball 'n' Roll-Android/Assets/Scripts/Main Menu Scripts/LoadSettingsScript.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class LoadSettingsScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-	//	ChangeSettings.SetQuality(PlayerPrefs.GetInt(ChangeSettings.qualityKey));
+		//ChangeSettings.SetQuality(PlayerPrefs.GetInt(ChangeSettings.qualityKey));
 		//ChangeSettings.SetVolume(PlayerPrefs.GetFloat("volume"));
-	//	ChangeSettings.SetFullScreen(System.Convert.ToBoolean(PlayerPrefs.GetInt("fullscreen")));
-    }
+		//SetFullScreen(System.Convert.ToBoolean(PlayerPrefs.GetInt("fullscreen")));
+		Permission.RequestUserPermission(Permission.ExternalStorageRead);
+		Permission.RequestUserPermission(Permission.ExternalStorageWrite);
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+	/*void SetFullScreen(bool isFullScreen)
+	{
+		if (isFullScreen)
+		{
+			//Screen.SetResolution(1920, 1080, true, 60);
+			Screen.fullScreen = true;
+		}
+		else
+		{
+			Screen.fullScreen = false;
+		}*/
+
+		//PlayerPrefs.SetInt("fullscreen", System.Convert.ToInt32(fullscreen.GetComponent<Toggle>().isOn));
+	}
+
 }
